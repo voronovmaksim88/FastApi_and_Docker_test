e5e4a40f13a0a9839dc6308e37c4d28d68a24fb4
voronovmaksim88 <voronovmaksim88@gmail.com>
2024-09-13 19:20:50 +0700

    add adress

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,  # type: ignore
-    allow_origins=["*"],
+    allow_origins=[
+         "http://localhost:5173",
+         "http://176.124.213.202",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -15,7 +18,7 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello, World!!!!"}
+    return {"message": "Hello, World from SibPLC !"}
 
 
 if __name__ == "__main__":
