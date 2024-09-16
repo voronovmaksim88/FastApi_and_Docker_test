124180c4ee5a12638a1ece8fc1f16dd4a6f5f91c
voronovmaksim88 <100327751+voronovmaksim88@users.noreply.github.com>
2024-09-16 11:48:18 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
 def home():
     """Домашняя страница"""
     html_content = "<h2>FastAPI is the best backend framework 16-09-2024 </h2>"
+    html_content = "<h2>правочки на сайте github </h2>"
     html_content += '<p>Интерактивная документация на <a href="/docs">/docs</a></p>'
     return HTMLResponse(content=html_content)
 
